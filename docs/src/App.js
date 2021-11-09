144b48071c419f2f1d44cad93933d4bf0b140acd
Abdull-Rahman <abed472011@gmail.com>
2021-11-09 20:13:09 +0200

    updates

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
-const App = () => <SwaggerUI url="api-documentation.json" />;
+const App = () => <SwaggerUI url="./api-documentation.json" />;
 
 export default App;
