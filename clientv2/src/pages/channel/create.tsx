11c7e74f2e51326a0cdb69a68549c984ca5c18b2
Abdull-Rahman <abed472011@gmail.com>
2022-01-23 06:32:29 +0200

    updates

---
+++ clientv2/src/pages/channel/create.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateChannel: React.FC = props => {
+
+    return (
+        <div className="create-channel-page">
+            hello
+        </div>
+    );
+
+}
+
+export default CreateChannel;
